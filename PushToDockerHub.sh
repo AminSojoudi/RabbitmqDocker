8fd03c752dcc3971f1288a54e88502c88d498baf
Amin Sojoudi <masojoudi@gmail.com>
2024-04-19 04:05:40 +0330

    Update PushToDockerHub.sh

--- PushToDockerHub.sh
+++ PushToDockerHub.sh
@@ -1,2 +1,5 @@
 docker build . -t docker.io/aminsojoudi/rabbitmq-managment-eventexchange:latest --platform linux/amd64
 docker push aminsojoudi/rabbitmq-managment-eventexchange:latest
+
+docker build . -t docker.io/aminsojoudi/rabbitmq-managment-eventexchange:latest-arm64 --platform linux/arm64/v8
+docker push aminsojoudi/rabbitmq-managment-eventexchange:latest-arm64
